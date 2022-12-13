6e96c592037b1b7e475ffd5efddf6dd0878ac9a2
nopjmp <kthompson@hey.com>
2022-12-12 21:32:24 -0600

    perf(CurrentlyShownController): Add to aggData.Listings instead of new
    We don't need to concat a new list if we are going to sort this after.

--- src/Universalis.Application/Controllers/CurrentlyShownControllerBase.cs
+++ src/Universalis.Application/Controllers/CurrentlyShownControllerBase.cs
@@ -69,7 +69,7 @@ public class CurrentlyShownControllerBase : WorldDcRegionControllerBase
                     // described in the property names (e.g. CreatorIdHash in the view and CreatorId in
                     // the database entity).
 
-                    aggData.Listings = next.Listings
+                    aggData.Listings.AddRange(next.Listings
                         .Select(l =>
                         {
                             if (!noGst)
@@ -82,11 +82,9 @@ public class CurrentlyShownControllerBase : WorldDcRegionControllerBase
                             l.WorldName = !worldDcRegion.IsWorld ? worlds[next.WorldId.Value] : null;
                             l.SerializableProperties = listingSerializableProperties;
                             return l;
-                        })
-                        .Concat(aggData.Listings)
-                        .ToList();
+                        }));
 
-                    aggData.RecentHistory = next.RecentHistory
+                    aggData.RecentHistory.AddRange(next.RecentHistory
                         .Where(s => entriesWithin < 0 || nowSeconds - s.TimestampUnixSeconds < entriesWithin)
                         .Select(s =>
                         {
@@ -94,9 +92,7 @@ public class CurrentlyShownControllerBase : WorldDcRegionControllerBase
                             s.WorldName = !worldDcRegion.IsWorld ? worlds[next.WorldId.Value] : null;
                             s.SerializableProperties = recentHistorySerializableProperties;
                             return s;
-                        })
-                        .Concat(aggData.RecentHistory)
-                        .ToList();
+                        }));
 
                     aggData.LastUploadTimeUnixMilliseconds = Math.Max(next.LastUploadTimeUnixMilliseconds, aggData.LastUploadTimeUnixMilliseconds);
 
