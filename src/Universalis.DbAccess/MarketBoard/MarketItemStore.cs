6ac802759eec6392177b6af67361941a26352b23
nopjmp <kthompson@hey.com>
2022-12-15 18:29:31 -0600

    remove MarketItemStore caching

--- src/Universalis.DbAccess/MarketBoard/MarketItemStore.cs
+++ src/Universalis.DbAccess/MarketBoard/MarketItemStore.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Cassandra;
+﻿using Cassandra;
 using Cassandra.Data.Linq;
 using Cassandra.Mapping;
 using Microsoft.Extensions.Logging;
-using StackExchange.Redis;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Universalis.Entities.MarketBoard;
 
 namespace Universalis.DbAccess.MarketBoard;
@@ -13,12 +12,10 @@ namespace Universalis.DbAccess.MarketBoard;
 public class MarketItemStore : IMarketItemStore
 {
     private readonly ILogger<MarketItemStore> _logger;
-    private readonly ICacheRedisMultiplexer _cache;
     private readonly IMapper _mapper;
-    
-    public MarketItemStore(ICluster cluster, ICacheRedisMultiplexer cache, ILogger<MarketItemStore> logger)
+
+    public MarketItemStore(ICluster cluster, ILogger<MarketItemStore> logger)
     {
-        _cache = cache;
         _logger = logger;
 
         var scylla = cluster.Connect();
@@ -46,18 +43,6 @@ public class MarketItemStore : IMarketItemStore
             _logger.LogError(e, "Failed to insert market item (world={WorldId}, item={ItemId})", marketItem.WorldId, marketItem.ItemId);
             throw;
         }
-
-        // Write through to the cache
-        var cache = _cache.GetDatabase(RedisDatabases.Cache.MarketItem);
-        var cacheKey = GetCacheKey(marketItem.WorldId, marketItem.ItemId);
-        try
-        {
-            await cache.StringSetAsync(cacheKey, marketItem.LastUploadTime.ToString(), flags: CommandFlags.FireAndForget);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Failed to store MarketItem \"{MarketItemCacheKey}\" in cache", cacheKey);
-        }
     }
 
     public async Task Update(MarketItem marketItem, CancellationToken cancellationToken = default)
@@ -76,73 +61,11 @@ public class MarketItemStore : IMarketItemStore
             _logger.LogError(e, "Failed to insert market item (world={WorldId}, item={ItemId})", marketItem.WorldId, marketItem.ItemId);
             throw;
         }
-
-        // Write through to the cache
-        var cache = _cache.GetDatabase(RedisDatabases.Cache.MarketItem);
-        var cacheKey = GetCacheKey(marketItem.WorldId, marketItem.ItemId);
-        try
-        {
-            await cache.StringSetAsync(cacheKey, marketItem.LastUploadTime.ToString());
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Failed to store MarketItem \"{MarketItemCacheKey}\" in cache", cacheKey);
-        }
     }
 
-    public async Task<MarketItem> Retrieve(int worldId, int itemId, CancellationToken cancellationToken = default)
+    public Task<MarketItem> Retrieve(int worldId, int itemId, CancellationToken cancellationToken = default)
     {
-        // Try to retrieve data from the cache
-        var cache = _cache.GetDatabase(RedisDatabases.Cache.MarketItem);
-        var cacheKey = GetCacheKey(worldId, itemId);
-        try
-        {
-            if (await cache.KeyExistsAsync(cacheKey))
-            {
-                var timestamp = await cache.StringGetAsync(cacheKey);
-                if (DateTime.TryParse(timestamp, out var t))
-                {
-                    return new MarketItem
-                    {
-                        WorldId = worldId,
-                        ItemId = itemId,
-                        LastUploadTime = t,
-                    };
-                }
-                else
-                {
-                    _logger.LogWarning("Failed to parse timestamp \"{RedisValue}\" for cached MarketItem \"{MarketItemCacheKey}\"", timestamp, cacheKey);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Failed to retrieve MarketItem \"{MarketItemCacheKey}\" from the cache", cacheKey);
-        }
-
         // Fetch data from the database
-        var match = await _mapper.FirstOrDefaultAsync<MarketItem>("SELECT * FROM market_item WHERE item_id=? AND world_id=?", itemId, worldId);
-        if (match == null)
-        {
-            return null;
-        }
-        
-        // Cache the result
-        try
-        {
-            await cache.StringSetAsync(cacheKey, match.LastUploadTime.ToString());
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Failed to store MarketItem \"{MarketItemCacheKey}\" in cache (t={LastUploadTime})", cacheKey, match.LastUploadTime);
-            throw;
-        }
-
-        return match;
-    }
-
-    private static string GetCacheKey(int worldId, int itemId)
-    {
-        return $"market-item:{worldId}:{itemId}";
+        return _mapper.FirstOrDefaultAsync<MarketItem>("SELECT * FROM market_item WHERE item_id=? AND world_id=?", itemId, worldId);
     }
 }
